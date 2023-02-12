7190f7046b5ac70aea8eb4bff51b0082612ab4d1
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-12 18:04:32 +0700

    Update views.py

--- tdlbc/views.py
+++ tdlbc/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-  template = loader.get_template('tdbc.html')
+  template = loader.get_template('tdlbc.html')
   return HttpResponse(template.render())
