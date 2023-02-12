b525240e517aa17b058040ad9924cd710db10e85
tijiop <jimmyop@gmail.com>
2023-02-12 15:28:04 +0700

    upload lai thu muc aichat vo github

--- aichat/settings.py
+++ aichat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["*']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tdlbc',
 ]
 
 MIDDLEWARE = [
---
+++ tdlbc/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+class bcbc(models.Model):
+       tents = models.CharField(max_length=800)
+       theloai = models.CharField(max_length=1200)
+       hieuqua = models.CharField(max_length=1500)
