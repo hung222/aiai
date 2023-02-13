d77a459d3742fb4393f9dad9b0bc53cfbb8cc36b
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-13 15:35:05 +0700

    Update settings.py

--- aichat/settings.py
+++ aichat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
-DEBUG = False
+DEBUG=False
 
 ALLOWED_HOSTS = ['*']
 
